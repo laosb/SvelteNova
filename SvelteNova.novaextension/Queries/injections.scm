554463eb6387d1d10e4952f5bbcd086b264b83cb
Shibo Lyu <github@of.sb>
2022-09-30 06:06:26 +0000

    feat: (Not working) injections.

---
+++ SvelteNova.novaextension/Queries/injections.scm
@@ -0,0 +1,31 @@
+; This query file is adopted from https://github.com/Himujjal/tree-sitter-svelte/blob/master/queries/injections.scm.
+; Nova's injection captures are different from the original one.
+
+(style_element
+  (raw_text
+    (#set! injection.language css)
+  ) @injection.content
+)
+
+; ((attribute
+;    (attribute_name) @_attr
+;    (quoted_attribute_value (attribute_value) @css))
+;  (#eq? @_attr "style"))
+; 
+; ((script_element
+;   (raw_text) @javascript))
+; 
+(raw_text_expr
+  (#set! injection.language javascript)
+) @injection.content
+
+(script_element
+	(start_tag
+	  (attribute
+      (attribute_name @_attr)
+		  (quoted_attribute_value (attribute_value) @injection.language)
+    )
+  )
+	(raw_text) @injection.content
+  ; (#eq? @_attr "lang"))
+)
