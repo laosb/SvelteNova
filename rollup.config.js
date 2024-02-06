2299cea970e724f3c6f114b259c035121d09daa8
Shibo Lyu <github@of.sb>
2024-02-06 22:28:18 +0800

    deps: Update all devDependencies.

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 
-module.exports = {
+export default {
   input: 'src/main.ts',
 
   plugins: [typescript(), commonjs(), nodeResolve()],
